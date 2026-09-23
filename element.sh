d70d10c338a11770c6dfca2c0d17b2f78fe19530
Leon <124583818+Trojanosos@users.noreply.github.com>
2026-09-23 09:01:20 -0400

    feat: output element info and handle unknown elements

--- element.sh
+++ element.sh
@@ -14,3 +14,13 @@ then
 else
   ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1'")
 fi
+
+if [[ -z $ELEMENT ]]
+then
+  echo "I could not find that element in the database."
+  exit
+fi
+
+IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$ELEMENT"
+
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
